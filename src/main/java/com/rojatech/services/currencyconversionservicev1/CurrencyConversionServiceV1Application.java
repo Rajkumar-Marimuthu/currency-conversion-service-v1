312d73adf306b23857f887f86f066108a490c0fe
Rajkumar-Marimuthu <rajkumartechgy@gmail.com>
2023-06-01 21:25:34 +0100

    create currency conversion service

---
+++ src/main/java/com/rojatech/services/currencyconversionservicev1/CurrencyConversionServiceV1Application.java
@@ -0,0 +1,13 @@
+package com.rojatech.services.currencyconversionservicev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServiceV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rojatech/services/currencyconversionservicev1/CurrencyConversionServiceV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rojatech.services.currencyconversionservicev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

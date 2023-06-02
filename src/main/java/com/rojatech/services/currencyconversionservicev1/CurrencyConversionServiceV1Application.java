bfc20468a9bfa8436bd592090dcea25218f43a51
Rajkumar-Marimuthu <rajkumartechgy@gmail.com>
2023-06-02 17:39:25 +0100

    use Spring Cloud Feign Client

--- src/main/java/com/rojatech/services/currencyconversionservicev1/CurrencyConversionServiceV1Application.java
+++ src/main/java/com/rojatech/services/currencyconversionservicev1/CurrencyConversionServiceV1Application.java
@@ -2,8 +2,10 @@ package com.rojatech.services.currencyconversionservicev1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrencyConversionServiceV1Application {
 
 	public static void main(String[] args) {

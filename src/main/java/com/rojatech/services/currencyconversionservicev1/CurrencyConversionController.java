b2ff966609f2629a722ad81e67e2b19b63db3897
Rajkumar-Marimuthu <rajkumartechgy@gmail.com>
2023-06-02 08:47:16 +0100

    consume currency exchange

--- src/main/java/com/rojatech/services/currencyconversionservicev1/CurrencyConversionController.java
+++ src/main/java/com/rojatech/services/currencyconversionservicev1/CurrencyConversionController.java
@@ -1,17 +1,30 @@
 package com.rojatech.services.currencyconversionservicev1;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
 	
+	
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
-		return new CurrencyConversion(100L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+		HashMap<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+		
+		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().
+				getForEntity("http://localhost:8000/currency-exchange/from/GBP/to/INR", CurrencyConversion.class, uriVariables);
+		CurrencyConversion currencyConversion = responseEntity.getBody();
+		return new CurrencyConversion(
+				currencyConversion.getId(), from, to, quantity, currencyConversion.getConversionMultiple(), 
+				quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());
 	}
 
 }
